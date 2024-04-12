dce53dc4a78d927e27f26a79c154ef9e504a906c
ilge.ustun <ilge.ustun@gmail.com>
2024-04-12 22:52:34 +0200

    fixes

--- packages/nextjs/app/fund-and-vote/page.tsx
+++ packages/nextjs/app/fund-and-vote/page.tsx
@@ -3,11 +3,7 @@
 import type { NextPage } from "next";
 
 const FundAndVote: NextPage = () => {
-  return (
-    <div className="container mx-auto my-10">
-      Fund and vote
-    </div>
-  );
+  return <div className="container mx-auto my-10">Fund and vote</div>;
 };
 
 export default FundAndVote;
