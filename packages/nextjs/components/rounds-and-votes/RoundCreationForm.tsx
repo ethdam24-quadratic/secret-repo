eb3bb06d77c41f07b813990da4cfcc2e97043518
arjanjohan <arjanvanhoogdalem@gmail.com>
2024-04-13 16:33:48 +0200

    form updated

--- packages/nextjs/components/rounds-and-votes/RoundCreationForm.tsx
+++ packages/nextjs/components/rounds-and-votes/RoundCreationForm.tsx
@@ -23,34 +23,36 @@ const projects = [
 ];
 
 const RoundCreationForm: React.FC = () => {
-  const [name, setName] = useState<string>("");
-  const [id, setId] = useState<string>("");
-  const [description, setDescription] = useState<string>("");
 
-  const [choosenProjects, setChoosenProjects] = useState<Project[]>([]);
+  const [roundTitle, setRoundTitle] = useState<string>("");
+  const [roundId, setRoundId] = useState<string>("");
+  const [roundDescription, setRoundDescription] = useState<string>("");
+  const [chosenProjects, setChosenProjects] = useState<Project[]>([]);
 
   const { address, connector } = useAccount();
 
   const CONTRACT_ADDRESS = "0xd2afe636a676aDF5Fd5CC414C95d3d45baF85954";
 
   const handleSubmit = async (event: React.FormEvent) => {
+    console.log("GM");
     event.preventDefault();
 
     if (!address || !connector) return;
     const provider = connector.options.getProvider();
 
     const functionArguments = {
-      id: "123",
-      name: "new funding round",
-      description: "a lot of good things",
-      projectIds: ["1", "2", "3"],
-      projectNames: ["first", "second", "third"],
-      projectDescriptions: ["first", "second", "third"],
+      id: roundId,
+      name: roundTitle,
+      description: roundDescription,
+      projectIds: chosenProjects.map(project => project.id),
+      projectNames: chosenProjects.map(project => project.name),
+      projectDescriptions: chosenProjects.map(project => project.description),
       payloadHash: "",
       routingInfo: "123",
       info: "info",
     };
 
+    console.log("AVH functionArguments: " + functionArguments);
     await submit(
       address,
       provider,
@@ -62,8 +64,7 @@ const RoundCreationForm: React.FC = () => {
       Number("123456"),
     );
   };
-
-  console.log(choosenProjects);
+  console.log(chosenProjects);
 
   return (
     <form className="flex flex-col w-full px-2" onSubmit={handleSubmit}>
@@ -71,8 +72,8 @@ const RoundCreationForm: React.FC = () => {
         <span className="label-text-alt">Round title</span>
         <input
           type="text"
-          value={name}
-          onChange={e => setName(e.target.value)}
+          value={roundTitle}
+          onChange={e => setRoundTitle(e.target.value)}
           placeholder="Type here"
           className="input input-bordered w-full rounded-none"
           required
@@ -83,8 +84,8 @@ const RoundCreationForm: React.FC = () => {
         <span className="label-text-alt">ID</span>
         <input
           type="text"
-          value={id}
-          onChange={e => setId(e.target.value)}
+          value={roundId}
+          onChange={e => setRoundId(e.target.value)}
           placeholder="Type here"
           className="input input-bordered w-full rounded-none"
           required
@@ -96,15 +97,15 @@ const RoundCreationForm: React.FC = () => {
         <textarea
           className="textarea textarea-bordered h-24 rounded-none"
           placeholder="Description"
-          value={description}
-          onChange={e => setDescription(e.target.value)}
+          value={roundDescription}
+          onChange={e => setRoundDescription(e.target.value)}
           required
         ></textarea>
       </label>
 
       <label className="form-control w-full my-1">
         <span className="label-text-alt">Projects</span>
-        <ProjectsSelect projects={projects} setChoosenProjects={setChoosenProjects} />
+        <ProjectsSelect projects={projects} setChoosenProjects={setChosenProjects} />
       </label>
 
       <button type="submit" className="btn rounded-none my-4">
