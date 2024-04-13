3c4cefe3590395a06ce9fdec26cd13862cd8f3aa
ilge.ustun <ilge.ustun@gmail.com>
2024-04-13 17:02:13 +0200

    separate projects file

---
+++ packages/nextjs/utils/quadratic/projects.ts
@@ -0,0 +1,23 @@
+const projects = [
+  {
+    id: "0",
+    name: "Legal Case of TC",
+    description: "Legal Defense Fund support for Alexey Pertsev and Roman Storm.",
+    imgSrc: "",
+  },
+  {
+    id: "1",
+    name: "Tor Project",
+    description: "Browse Privately. Explore Freely. We’re a nonprofit defending your privacy and freedom online.",
+    imgSrc: "",
+  },
+  {
+    id: "2",
+    name: "web3privacy-now",
+    description:
+      "Privacy advocates worldwide are coming together to discuss how to mainstream privacy within the Web3 industry.",
+    imgSrc: "",
+  },
+];
+
+export { projects };
